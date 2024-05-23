099509195d2f5c4299faa822d194b2b6a9a2e82f
aqmalProjek <aqmalprojek2001@gmail.com>
2024-05-23 09:23:44 +0700

    fix button for main failed pr

--- components/buttons/main-buttons/index.jsx
+++ components/buttons/main-buttons/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function MainButton({text,type = 'button',onClick}) {
+export default function MainButton({text,type = 'button',onClick, className}) {
   return (
-    <button className="border border-black px-5 py-3 rounded bg-violet-900 text-white" type={type} onClick={onClick}>{text}</button>
+    <button className={"border border-black px-5 py-3 rounded bg-violet-900 text-white " + className} type={type} onClick={onClick}>{text}</button>
   )
 }
