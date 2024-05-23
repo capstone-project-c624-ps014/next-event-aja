c4ce368ed2020cf86ec495c7cfda54fea1598478
aqmalProjek <aqmalprojek2001@gmail.com>
2024-05-23 09:21:48 +0700

    fix button

--- components/buttons/secondary-button/index.jsx
+++ components/buttons/secondary-button/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function SecondaryButton({text,type = 'button',onClick, className}) {
+export default function SecondaryButton({text,type = 'button',onClick}) {
   return (
-    <button className={"border border-black px-5 py-3 rounded bg-fuchsia-800 text-white " + className} type={type} onClick={onClick}>{text}</button>
+    <button className="border border-black px-5 py-3 rounded bg-fuchsia-800 text-white" type={type} onClick={onClick}>{text}</button>
   )
 }
